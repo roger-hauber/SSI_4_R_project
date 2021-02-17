a2cdeddcc72a856f38fa576d1d0032a2cd651127
Roger Hauber <roger.hauber@posteo.de>
2021-02-17 19:01:33 +0100

    first couple of lines in descriptives_script

--- in_depth_descriptives.R
+++ in_depth_descriptives.R
@@ -1,4 +1,7 @@
 ################# FUUUCK
 
 #okay once again from the top
+# first things first: pre-post variable for a4 data
+a4[a4$subject %in% c(3,4, 7:18), "lockdown"] <- "pre"
+a4[a4$subject %in% c(1,2,5,6, 18:28), "lockdown"] <- "post"
 
